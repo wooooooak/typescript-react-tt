cce9f6fe267ffcd0cc1d43849ec01e46258f0672
DESKTOP-RF047AL\wooooooak <wooooooak@gmail.com>
2018-07-08 06:19:55 +0900

    made counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
-import './App.css';
 
-import logo from './logo.svg';
+import Counter from './components/Counter';
+import Profile from './components/Profile';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Profile name="whyj" job="developer" />
+        <Counter />
       </div>
     );
   }
